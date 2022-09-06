d99bcea9da8bdd858ef838aaf30b5eebf950931d
Francisco Briceño S <fjavier.cgam@gmail.com>
2022-09-06 16:33:07 -0600

    Deleted the default code on App.js

--- src/App.js
+++ src/App.js
@@ -11,24 +11,9 @@ import GreetingStyled from './components/pure/forms/greetingStyled';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <TaskList></TaskList>
         <ComponenteConContexto></ComponenteConContexto>
         <GreetingStyled name="Francisco"></GreetingStyled>
-      </header>
-      
     </div>
   );
 }
