c30dee8896f433dd2856a4b6010005edb63e9bd9
Francisco Briceño S <fjavier.cgam@gmail.com>
2022-09-06 16:36:39 -0600

    Se agrego badge a las tareas

--- src/components/pure/Task.jsx
+++ src/components/pure/Task.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import TaskModel from '../../models/task-model'
 import "../../styles/task.scss"
+import { LEVELS } from '../../models/levels';
 function Task({task}) {
 
     useEffect(() => {
@@ -13,6 +14,41 @@ function Task({task}) {
         };
     }, [task]);
 
+    /**
+     * Return badge depending on the level of the task
+     */
+    function taskLevelBadge(){
+        switch(task.level){
+            case LEVELS.NORMAL:
+                return(
+                    <h6 className='mb-0'>
+                        <span className='badge bg-primary'>
+                            {task.level}
+                        </span>
+                    </h6>
+                );                
+            case LEVELS.URGENT:
+                return(
+                    <h6 className='mb-0'>
+                        <span className='badge bg-warning'>
+                            {task.level}
+                        </span>
+                    </h6>
+                );
+                case LEVELS.BLOCKING:
+                return(
+                    <h6 className='mb-0'>
+                        <span className='badge bg-danger'>
+                            {task.level}
+                        </span>
+                    </h6>
+                );
+                default:
+                    break;
+            
+        }
+    }
+
   return (
         <tr>
             <td>
@@ -22,14 +58,21 @@ function Task({task}) {
                 <span>{task.description}</span> 
             </td>
             <td className='align-middle'>
-                <span>{task.isDone ? "Completado" : "Pendiente"}</span>                
+                <span>{renderCompleted()}
+                    <i className='bi-trash' style={{color:"tomato"}}></i>
+                </span>                
             </td>
             <td className='align-middle'>
-                
-                <span>{task.level}</span>    
+                <span>{taskLevelBadge()}</span>    
             </td>
         </tr>   
   )
+
+    function renderCompleted() {
+        return task.isDone
+            ? (<i className='bi-toggle-on' style={{ color: "green" }}></i>)
+            : (<i className='bi-toggle-off' style={{ color: "red" }}></i>);
+    }
 }
 
 Task.propTypes = {
